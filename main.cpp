df52c074017bb70dd324256d9b0361f4960ef915
FlightlessMango <flightlessmangoyt@gmail.com>
2020-01-06 22:57:43 +0100

    Use set instead of vector

--- main.cpp
+++ main.cpp
@@ -5,32 +5,34 @@
 #include <vector>
 #include "X11/keysym.h"
 #include <chrono>
+#include <set>
 // extern "C" {
 //     #include <xdo.h>
 // }
 
 auto last_change = std::chrono::high_resolution_clock::now();
 auto now = std::chrono::high_resolution_clock::now();
-std::vector<XEvent> keyArray;
-std::vector<XEvent> keyArray_prev;
+std::set<unsigned int> keyArray;
+std::set<unsigned int> keyArray_prev;
 // xdo_t * x = xdo_new(":0.0");
 Display *display = XOpenDisplay(getenv("DISPLAY"));
 
 void writeToFile(){
-    printf("%i\n", keyArray_prev.size());
+    printf("%i\n", keyArray.size());
     FILE *outFile = fopen("/tmp/recording", "a");
     now = std::chrono::high_resolution_clock::now();  
     if (keyArray.empty()){
         fprintf(outFile, "%i\n", std::chrono::duration_cast<std::chrono::microseconds>(now - last_change));
         fflush(outFile);
     }
+    if (!keyArray_prev.empty()){
+        fprintf(outFile, "%i\n", std::chrono::duration_cast<std::chrono::microseconds>(now - last_change));
+        fflush(outFile);
+    }
 
-    fprintf(outFile, "%i\n", std::chrono::duration_cast<std::chrono::microseconds>(now - last_change));
-    fflush(outFile);
-
-    for (size_t i = 0; i < keyArray.size(); i++)
+    for (auto i : keyArray)
     {
-        KeySym ks = XKeycodeToKeysym(display, keyArray[i].xkey.keycode, 1);
+        KeySym ks = XKeycodeToKeysym(display, i, 1);
         if (i == keyArray.size() - 1){
             fprintf(outFile, "%s\n", XKeysymToString(ks));
         } else {
@@ -63,7 +65,6 @@ int main()
     while (1)
     {   
         keyArray_prev = keyArray;
-        now = std::chrono::high_resolution_clock::now();
     // auto elapsed = std::chrono::high_resolution_clock::now() - start;
 
     // long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
@@ -72,17 +73,13 @@ int main()
         /* keyboard events */
         if (event.type == KeyPress)
         {
-            keyArray.push_back(event);
+            keyArray.insert(event.xkey.keycode);
             printf( "KeyPress: %x\n", event.xkey.keycode );
             writeToFile();
         }
         else if (event.type == KeyRelease)
         {
-            for (size_t i = 0; i < keyArray.size(); i++)
-            {
-                if (keyArray[i].xkey.keycode == event.xkey.keycode)
-                    keyArray.erase(keyArray.begin() + i);
-            }
+            keyArray.erase(event.xkey.keycode);
             printf( "KeyRelease: %x\n", event.xkey.keycode );
             writeToFile();
         }
